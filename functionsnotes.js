f8bd164c8af8d3b1a2dfd468e39dc473463a051c
8nau <8nau@fresnoU.org>
2023-03-14 10:01:54 -0700

    initial.commit

---
+++ functionsnotes.js
@@ -0,0 +1,39 @@
+//Functions Notes
+//Step 1. Define the function
+
+function sayHello(){
+
+console.log('Hello');
+
+ }
+
+//2. call or use the function
+
+ sayHello();
+//define
+
+function printMax(){
+
+let x = 6;
+let y = 29;
+ if (x>y){
+  console.log(`the larger number is ${x}`);
+ } else {
+ console.log(`the larger number is ${y}`);
+ }
+ }
+ call
+
+printMax();
+
+if (x>y){
+
+console.log(`the larger number is ${x}`);
+
+ } else {
+
+console.log(`the larger number is ${y}`);
+ }
+printMax(16, 9);
+printMax(2, 75);
+printMax(2, 2);
