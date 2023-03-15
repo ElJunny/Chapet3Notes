56979077bfb79969493e4fb011b2ab79d12b5c8f
8nau <8nau@fresnoU.org>
2023-03-15 10:29:39 -0700

    initial.commit

---
+++ functionexcercises.js
@@ -0,0 +1,14 @@
+//AreaOfRectangle
+// function product(num1,num2){
+//     console.log(`${num1}*${num2})=$(num1*num2)`);
+// }
+// product(10,2)
+// product(10,2)
+
+//Area if circle
+
+// function product(num1,num2){
+//     console.log(`${num1}*${num2})=$(num1*num2)`);
+// }
+
+//findMin
---
+++ functions1.js
@@ -0,0 +1,53 @@
+// //Find the area of a rectangle
+// function product(num1, num2){
+// console.log(`${num1}*${num2}=${num1*num2}`);
+// }
+// product(10, 2);
+// product(10, 2);
+//
+// //Find the area of a circle
+// function product(radius, pi){
+//     console.log(`${pi} * ${radius} * ${radius}
+//     = ${radius*pi}`); }
+// product(6, 3.14);
+// product(6, 3.14);
+
+//findMin
+//  function printMax(x, y){
+//       if (x<y){
+//           console.log(`the smaller number is ${x}`);}
+//  } else {
+//     console.log(`the smaller number is ${y}`);
+//
+// }
+// printMax (16, 9);
+// printMax(3, 75);
+// printMax(2, 5);
+
+//checkEquality
+// function printMax(x, y){
+// if (x==y){
+// console.log(`${x} and ${y} are equal`);
+// } else {
+// console.log(`${x} and ${y} are not equal`);
+// }
+// }
+// printMax (16, 9);
+// printMax(75, 75);
+// printMax(4.9, 5);
+
+//calculate
+// function product(rec1, rec2){
+//     console.log(`${rec1}*${rec2}=${rec1*rec2}`);
+// }
+// product(10, 2);
+// product(3, 11);
+// function printMax(rec1, rec2){
+// if (rec1>rec2){
+// console.log(`the bigger rectangle is ${rec1}`);
+// } else {// console.log(`the bigger rectangle is ${rec2}`);
+// }
+// }
+.
+// printMax (10, 2);
+// printMax(3, 11);
