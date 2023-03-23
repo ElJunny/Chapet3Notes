6707bbfeade7e59a555499e8d277a061d3a751bf
8nau <8nau@fresnoU.org>
2023-03-23 10:29:53 -0700

    initial.commit

---
+++ arrays1.js
@@ -0,0 +1,14 @@
+//prints the second value in an array
+function one(arr){
+
+    console.log( arr[1] )
+
+}
+one( [`a`, `b`, `c`, `d`] )
+one( [7, 5, 3, 1] )
+
+function lastlelement(arr) {
+    console.log(arr[arr.length - 1])
+}lastlelement([`a`,`b`,`c`,`d`])
+lastlelement([7,5,3,1])
+
