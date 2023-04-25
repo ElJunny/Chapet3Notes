9d75524404b2823c49009fb37ff36c5f53fbaf82
8nau <8nau@fresnoU.org>
2023-04-25 09:40:06 -0700

    initial.commit

--- Combo.js
+++ Combo.js
@@ -4,11 +4,11 @@ let sandwich = readLine.question(`Hello, what sandwich would you like to start w
 console.log(`Alright a ${sandwich} sandwich`);
 let answer = readLine.question(`Would you like a beverage with that  :`)
 
-while (answer != "Yes" && answer !="No") {
+{
     if (answer = "Yes") {
         let beverage = readLine.question("What size would you like it in  :")
     }
-    if (answer = "No") {
+    if (answer = "No" ) {
         readLine.question("Okay!")
     }
 }
